9a78699784eb62b275c28b5a48e52572c4316f25
johns31 <rajeshkadavil@hotmail.com>
2017-02-16 13:14:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UIDGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("started...");
+
+            //Test2();
+            int inputId = GetUnique(1, 2);
+            string code = GetEncode(inputId);
+            Console.WriteLine("Input : " + inputId);
+            Console.WriteLine("Encoded value : " + code);
+            var input = GetDecode(code);
+            Console.WriteLine("Decoded value : " + input);
+            Console.WriteLine("End !!!");
+            Console.ReadLine();
+        }
+
+        private static void Test1()
+        {
+            String[] codes = new string[10000];
+
+            UniqueIdGenerator uniqueIdGenerator = new UniqueIdGenerator();
+            for (int i = 0; i < 10000; i++)
+            {
+                var now = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture);
+                string input = string.Concat(i, now).Replace(" ", "");
+
+                codes[i] = uniqueIdGenerator.GetUniqueIdentifier(input);
+                //codes[i] = uniqueIdGenerator.GetGuid(i);
+                //codes[i] = uniqueIdGenerator.GetBase62UniqueIdentifier("Rajesh John" + i, "31/05/1980");
+            }
+
+            for (int i = 0; i < codes.Length; i++)
+            {
+                for (int j = i + 1; j < codes.Length; j++)
+                {
+                    if (codes[i] == codes[j])
+                    {
+                        Console.WriteLine("Duplicates !!!!!");
+                        Console.WriteLine("item on " + i + " : " + codes[i]);
+                        Console.WriteLine("item on " + j + " : " + codes[j]);
+                    }
+                }
+                // Console.WriteLine("code :" + codes[i]);
+            }
+        }
+
+        private static void Test2()
+        {
+            var codes = new List<Tuple<string, int, int>> ();
+
+            foreach (var pair in PayoutData.GetData())
+            {
+                var code = Encoder.Conceal(GetUnique(pair.Key,pair.Value));
+                codes.Add(new Tuple<string, int, int>(code,pair.Key,pair.Value));
+            }
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                for (int j = i + 1; j < codes.Count; j++)
+                {
+                    if (codes[i].Item1 == codes[j].Item1)
+                    {
+                        Console.WriteLine("Duplicates !!!!!");
+                        Console.WriteLine("item on " + i + " : " + codes[i]);
+                        Console.WriteLine("item on " + j + " : " + codes[j]);
+
+                        Console.WriteLine($"Duplicate Code {codes[i].Item1}, payout id {codes[i].Item2}, player {codes[i].Item3}");
+                        Console.WriteLine($"Duplicate Code {codes[j].Item1}, payout id {codes[j].Item2}, player {codes[j].Item3}");
+
+                    }
+                }
+
+                //Console.WriteLine("code :" + codes[i]);
+            }
+        }
+
+        private static int GetUnique(int a, int b)
+        {
+            int result = (a + b)*(a + b + 1);
+            result = (int)Decimal.Multiply(result, 0.5m);
+            result += b;
+            if (result < 0)
+                result *= -1;
+
+            return result;
+        }
+
+        private static string GetEncode(int input)
+        {
+            var code = Encoder.Conceal(input);
+            return code;
+        }
+
+        private static int GetDecode(string code)
+        {
+            var input = Encoder.Reveal(code);
+            return input;
+        }
+    }
+}
---
+++ UniqueIdGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UIDGenerator
+{
+    public sealed class UniqueIdGenerator
+    {
+        private readonly object _lock = new object();
+        private const int MaxLength = 6;
+
+        public string GetUniqueIdentifier(string input)
+        {
+            var stringBuilder = new StringBuilder();
+
+            lock (_lock)
+            {
+                var hashString = GetHash(input);
+                foreach (char chr in hashString)
+                {
+                    if(!stringBuilder.ToString().Contains(chr) && chr != '0' && chr != 'o' && chr != '1' && chr != 'I')
+                        stringBuilder.Append(chr);
+                    if (stringBuilder.ToString().Length == MaxLength)
+                        break;
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+        private string GetHash(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return String.Empty;
+
+            using (var sha = new SHA256Managed())
+            {
+                byte[] inputData = Encoding.UTF8.GetBytes(input);
+                byte[] hash = sha.ComputeHash(inputData);
+                return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+        }
+
+        #region other
+        public string GetGuid(int id)
+        {
+            var now = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture);
+            string input = string.Concat(id, now).Replace(" ", "");
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(input));
+        }
+
+        public string GetBase62UniqueIdentifier(string name, string dob)
+        {
+            var now = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture);
+            string input = string.Concat(name, dob, now).Replace(" ", "");
+            var stringBuilder = new StringBuilder();
+
+            lock (_lock)
+            {
+                var hashString = GetHash(input);
+                string base62Text = GetBase62(hashString);
+                foreach (char chr in base62Text)
+                {
+                    if (!stringBuilder.ToString().Contains(chr) && chr != '0' && chr != 'o' && chr != '1' && chr != 'I')
+                        stringBuilder.Append(chr);
+                    if (stringBuilder.ToString().Length == MaxLength)
+                        break;
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+        private static Random _random = new Random();
+
+        private static string GetBase62(string inputText)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < inputText.Length; i++)
+                sb.Append(inputText[_random.Next(62)]);
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
